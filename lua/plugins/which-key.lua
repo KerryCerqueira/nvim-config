f4de11139dd9c89f6bd70894f7e31f279c3527ac
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-02 11:55:47 -0500

    Add which-key plugin.

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return { -- which-key
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		plugins = { spelling = true },
+	},
+}
