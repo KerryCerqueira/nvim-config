c26ef18ebbd5dae3accf92d3faa9f447c299845d
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-02 11:54:10 -0500

    Add plugins for better folding.

---
+++ lua/plugins/folding.lua
@@ -0,0 +1,15 @@
+return {
+	{ -- pretty-fold
+		"anuvyklack/pretty-fold.nvim",
+		opts = {},
+		version = false,
+	},
+	{ -- fold-preview
+		"anuvyklack/fold-preview.nvim",
+		dependencies = {
+			"anuvyklack/keymap-amend.nvim",
+		},
+		opts = {},
+		version = false,
+	},
+}
