f02fdcc8c132180d563162ffd55b7b846307b148
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-04 00:05:24 -0500

    add lang import to init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,20 @@
 vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+	{import = "plugins"},
+	{import = "plugins.lang"},
+})
+vim.opt.shiftwidth = 3
+vim.opt.tabstop = 3
+print("test")
