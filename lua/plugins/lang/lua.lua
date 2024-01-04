24b02c4628c74883417b984d7bcfc42f9b7d2bb6
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-03 23:56:16 -0500

    Add lua lsp configuration and neodev for lua development.

---
+++ lua/plugins/lang/lua.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "folke/neodev.nvim", opts = {} },
+		},
+		opts = {
+			servers = {
+				lua_ls = {
+					on_attach = function(_, bufnr)
+						require("keymaps").set_keymaps(
+							require("keymaps.lsp").common,
+							bufnr
+						)
+					end,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "lua_ls" })
+		end,
+	},
+}
