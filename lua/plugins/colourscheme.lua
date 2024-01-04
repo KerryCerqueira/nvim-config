7abf7244fff7d1e139976c80800eaf371ae3e514
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-04 00:06:23 -0500

    Add default colorscheme behaviour

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -2,8 +2,6 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("tokyonight").setup()
-		vim.cmd.colorscheme('tokyonight-night')
-	end,
+	init = function() vim.cmd.colorscheme('tokyonight-night') end,
+	opts = {}
 }
