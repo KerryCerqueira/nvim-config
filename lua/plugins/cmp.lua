6554a54b19f075d7cf706bdb4360791dfa057f17
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-04 00:05:49 -0500

    Add luasnippets tab navigation.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,12 +7,44 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"saadparwaiz1/cmp_luasnip",
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
+		{
+			"L3MON4D3/LuaSnip",
+			keys = {
+				{
+					"<tab>",
+					function()
+						return (
+							require("luasnip").jumpable(1)
+							and "<Plug>luasnip-jump-next"
+							or "<tab>"
+						)
+					end,
+					expr = true, silent = true, mode = "i",
+				},
+				{
+					"<tab>",
+					function() require("luasnip").jump(1) end,
+					mode = "s"
+				},
+				{
+					"<s-tab>",
+					function() require("luasnip").jump(-1) end,
+					mode = { "i", "s" }
+				},
+			},
+		},
+		{
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		}
 	},
 	opts = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
-		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+		vim.api.nvim_set_hl(
+			0, "CmpGhostText",
+			{ link = "Comment", default = true }
+		)
 		local cmp = require("cmp")
 		local defaults = require("cmp.config.default")()
 		return {
@@ -25,8 +57,12 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-n>"] = cmp.mapping.select_next_item(
+					{ behavior = cmp.SelectBehavior.Insert }
+				),
+				["<C-p>"] = cmp.mapping.select_prev_item(
+					{ behavior = cmp.SelectBehavior.Insert }
+				),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
