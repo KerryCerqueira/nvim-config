ebc5045351df9f10781b3aba77e32c6b79b9ced5
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-04 00:29:59 -0500

    Add mini.pairs disable key.

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -95,8 +95,23 @@ return {
 	},
 	{ -- mini.pairs
 		'echasnovski/mini.pairs',
+		version = false,
+		keys = {
+			{
+				"<leader>up",
+				function()
+					local Util = require("lazy.core.util")
+					vim.g.minipairs_disable = not vim.g.minipairs_disable
+					if vim.g.minipairs_disable then
+						Util.warn("Disabled auto pairs", { title = "Option" })
+					else
+						Util.info("Enabled auto pairs", { title = "Option" })
+					end
+				end,
+				desc = "Toggle auto pairs",
+			},
+		},
 		opts = {},
-		version = false
 	},
 	{ -- mini.trailspace
 		'echasnovski/mini.trailspace',
