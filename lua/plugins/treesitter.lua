1c6f2f218e03265dc8068e1cd8aa441af311b985
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-07 18:29:42 -0500

    Clean up extraneous parsers, which should be added by language specific specs.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,16 @@
-return { -- treesitter
+return {
 	"nvim-treesitter/nvim-treesitter",
 	version = false,
 	event = { "BufReadPre", "BufNewFile" },
 	build = function()
 		require("nvim-treesitter.install").update({ with_sync = true })()
 	end,
-	-- init = function(plugin)
-	-- 	-- PERF: add nvim-treesitter queries to the rtp and its custom query
-	-- 	-- predicates early This is needed because a bunch of plugins no
-	-- 	-- longer `require("nvim-treesitter")`, which no longer trigger the
-	-- 	-- **nvim-treeitter** module to be loaded in time. Luckily, the only
-	-- 	-- things that those plugins need are the custom queries, which we
-	-- 	-- make available during startup.
-	-- 	require("lazy.core.loader").add_to_rtp(plugin)
-	-- 	require("nvim-treesitter.query_predicates")
-	-- end,
 	opts = {
 		ensure_installed = {
-			"python", "lua", "html", "css", "bash", "bibtex", "c", "cmake",
-			"cpp", "csv", "diff", "git_config", "git_rebase", "gitattributes",
-			"gitcommit", "gitignore", "java", "json", "latex", "luap", "luau",
-			"luadoc", "make", "markdown", "markdown_inline", "perl", "regex",
-			"pymanifest", "requirements", "rust", "sql", "ssh_config", "toml",
-			"tsv", "vim", "vimdoc", "xml", "yaml",
+			"html", "bash", "csv", "diff", "git_config", "git_rebase",
+			"gitattributes", "gitcommit", "gitignore", "json", "latex",
+			"markdown", "markdown_inline", "regex", "ssh_config", "toml", "tsv",
+			"vim", "vimdoc", "xml", "yaml",
 		},
 		highlight = { enable = true },
 		indent = { enable = true },
