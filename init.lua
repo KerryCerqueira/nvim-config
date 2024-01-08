32522c73951a43d8b6b14f98a5d708444a4aacb7
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-07 23:16:06 -0500

    Add neovide options.

--- init.lua
+++ init.lua
@@ -20,3 +20,13 @@ vim.opt.tabstop = 3
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
+if vim.g.neovide then
+	vim.o.guifont = "MesloLGS NF:h11"
+	vim.g.neovide_transparency = 0.8
+	vim.g.neovide_floating_blur_amount_x = 0
+	vim.g.neovide_floating_blur_amount_y = 0
+	vim.g.neovide_floating_shadow = true
+	vim.g.neovide_floating_z_height = 10
+	vim.g.neovide_light_angle_degrees = 45
+	vim.g.neovide_light_radius = 5
+end
