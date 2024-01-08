6f68d2f5399658d6b9b05116e1ed5041b1a03e2a
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-08 01:12:48 -0500

    Fix venv-selector.

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -43,9 +43,27 @@ return {
 		end,
 	},
 	{
+		"mfussenegger/nvim-dap",
+		optional = true,
+		dependencies = {
+			"mfussenegger/nvim-dap-python",
+			-- stylua: ignore
+			keys = {
+				{ "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+				{ "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+			},
+			config = function()
+				local path = require("mason-registry").get_package("debugpy"):get_install_path()
+				require("dap-python").setup(path .. "/venv/bin/python")
+			end,
+		},
+	},
+	{
 		"linux-cultist/venv-selector.nvim",
+		dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
 		cmd = "VenvSelect",
 		opts = {
+			dap_enabled = true,
 			name = {
 				"venv",
 				".venv",
