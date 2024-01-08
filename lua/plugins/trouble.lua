ae5627d48d0aa9e2272c3ecc86b2861269b5721f
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-07 23:16:33 -0500

    Add which-key integration to trouble.

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,64 @@
+return {
+	"folke/trouble.nvim",
+	cmd = { "TroubleToggle", "Trouble" },
+	opts = { use_diagnostic_signs = true },
+	keys = {
+		{
+			"<Leader>xx",
+			"<cmd>TroubleToggle document_diagnostics<cr>",
+			desc = "Document Diagnostics (Trouble)"
+		},
+		{
+			"<Leader>xX",
+			"<cmd>TroubleToggle workspace_diagnostics<cr>",
+			desc = "Workspace Diagnostics (Trouble)"
+		},
+		{
+			"<Leader>xL",
+			"<cmd>TroubleToggle loclist<cr>",
+			desc = "Location List (Trouble)"
+		},
+		{
+			"<Leader>xQ",
+			"<cmd>TroubleToggle quickfix<cr>",
+			desc = "Quickfix List (Trouble)"
+		},
+		{
+			"[q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").previous({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cprev)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Previous trouble/quickfix item",
+		},
+		{
+			"]q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").next({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cnext)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Next trouble/quickfix item",
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		optional = true,
+		opts = {
+			defaults = {
+				["<Leader>x"] = { name = "+trouble" },
+			},
+		},
+	},
+}
