84b8ab51fd838665c2a061e00195e2766bb05f33
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-01-08 20:18:29 -0500

    Add floaterm plugin

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,25 @@
+return {
+	"voldikss/vim-floaterm",
+	keys = {
+		{
+			"<Leader>tt",
+			"<cmd> FloatermToggle<cr>",
+			desc = "Toggle terminal",
+		},
+		{
+			"<Leader>tT",
+			"<cmd> FloatermNew<cr>",
+			desc = "Start new terminal",
+		},
+		{
+			"<Leader>tn",
+			"<cmd> FlatermNext<cr>",
+			desc = "Next terminal",
+		},
+		{
+			"<Leader>tp",
+			"<cmd> FlatermPrev<cr>",
+			desc = "Previous terminal",
+		},
+	},
+}
