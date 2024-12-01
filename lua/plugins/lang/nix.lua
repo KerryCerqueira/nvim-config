88a01fc7dcd3bd5e81912b59d27df39b647e56db
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-12-01 17:40:12 -0500

    Roll back nixd use for nil_ls

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -3,22 +3,7 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				nixd = {
-					nixpkgs = {
-						expr = "import <nixpkgs> { }",
-					},
-					formatting = {
-						command = { "alejandra" },
-					},
-					options = {
-						nixos = {
-							expr = "nixpkgs#nixos-options";
-						},
-						home_manager =  {
-							expr =  "(import <home-manager/modules> { configuration = ~/.config/home-manager/home.nix; pkgs = import <nixpkgs> {}; }).options"
-						},
-					},
-				},
+				nil_ls = {},
 			},
 		},
 	},
