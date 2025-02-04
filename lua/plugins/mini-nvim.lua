557a03f10e46b44806f1bd05d90da0518cced1e1
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-02-03 20:52:29 -0500

    Update mini module
    Remove animate, indentscope (replace with snacks). Remove starter (never
    used it). Add basics, align, splitjoin, jump.

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,38 +1,73 @@
 return {
-	{ -- indentscope
-		"echasnovski/mini.indentscope",
+	{
+		'echasnovski/mini.ai',
 		event = "BufEnter",
-		version = false,
-		opts = {
-			symbol = "▏",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help", "alpha", "dashboard", "neo-tree", "Trouble", "trouble",
-					"lazy", "notify", "toggleterm", "lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
+		opts = {},
+		version = false
 	},
-	{ -- mini.map
+	-- TODO: Decide on keymaps
+	{
+		'echasnovski/mini.align',
+		event = "BufEnter",
+		opts = {},
+		version = false
+	},
+	{
+		'echasnovski/mini.basics',
+		lazy = true,
+		opts = {
+			extra_ui = true,
+			mappings = {
+				windows = true,
+			},
+			autocommands = { relnum_in_visual_mode = true, },
+		},
+		version = false
+	},
+	{
+		'echasnovski/mini.bracketed',
+		event = "BufEnter",
+		opts = {},
+		version = false
+	},
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = { enable_autocmd = false },
+		},
+		opts = {
+			custom_commentstring = function()
+				local module = require("ts_context_commentstring")
+				return module.calculate_commentstring() or vim.bo.commentstring
+			end,
+		},
+	},
+	{
+		'echasnovski/mini.jump',
+		event = "BufEnter",
+		opts = {},
+		version = false
+	},
+	{
 		'echasnovski/mini.map',
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"folke/snacks.nvim",
+		},
 		version = false,
 		event = "BufEnter",
 		keys = {
 			{
-				"<Leader>mm",
+				"\\mm",
 				function()
 					require("mini.map").toggle()
 				end,
 				desc = "Toggle minimap",
 			},
 			{
-				"<Leader>mf",
+				"\\mf",
 				function()
 					require("mini.map").toggle_focus()
 				end,
@@ -45,81 +80,59 @@ return {
 				callback = require("mini.map").open,
 			})
 		end,
-		config = function()
-			local map = require('mini.map')
-			map.setup({
-				symbols = {encode = map.gen_encode_symbols.dot("4x2")},
-				integrations = {
-					map.gen_integration.builtin_search(),
-					map.gen_integration.diagnostic(),
-					map.gen_integration.gitsigns(),
-				},
-			})
-		end,
-	},
-	{ -- mini.comment
-		"echasnovski/mini.comment",
-		version = false,
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			opts = {enable_autocmd = false},
-		},
 		opts = {
-			custom_commentstring = function()
-				return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
-			end,
+			symbols = {encode = require("mini.map").gen_encode_symbols.dot("4x2")},
+			integrations = {
+				require("mini.map").gen_integration.builtin_search(),
+				require("mini.map").gen_integration.diagnostic(),
+				require("mini.map").gen_integration.gitsigns(),
+			},
 		},
 	},
-	{ -- mini.surround
-		'echasnovski/mini.surround',
-		event = "BufEnter",
-		opts = {},
-		version = false
-	},
-	{ -- mini.bracketed
-		'echasnovski/mini.bracketed',
-		event = "BufEnter",
-		opts = {},
-		version = false
-	},
-	{ -- mini.starter
-		'echasnovski/mini.starter',
-		opts = {},
-		version = false
-	},
-	{ -- mini.pairs
+	{
 		'echasnovski/mini.pairs',
 		event = "BufEnter",
 		version = false,
 		keys = {
 			{
-				"<Leader>up",
+				"<Leader>tp",
 				function()
-					local Util = require("lazy.core.util")
 					vim.g.minipairs_disable = not vim.g.minipairs_disable
-					if vim.g.minipairs_disable then
-						Util.warn("Disabled auto pairs", { title = "Option" })
-					else
-						Util.info("Enabled auto pairs", { title = "Option" })
-					end
 				end,
 				desc = "Toggle auto pairs",
 			},
 		},
 		opts = {},
 	},
-	{ -- mini.trailspace
+	{
+		"echasnovski/mini.splitjoin",
+		event = "BufEnter",
+		version = false,
+		opts = {},
+	},
+	{
+		"echasnovski/mini.surround",
+		event = "BufEnter",
+		opts = {
+			mappings = {
+				add = '<Leader>sa',
+				delete = '<Leader>sd',
+				find = '<Leader>sf',
+				find_left = '<Leader>sF',
+				highlight = '<Leader>sh',
+				replace = '<Leader>sr',
+				update_n_lines = '<Leader>sn',
+				suffix_last = 'l',
+				suffix_next = 'n',
+			},
+		},
+		version = false
+	},
+	{
 		'echasnovski/mini.trailspace',
 		event = "BufEnter",
 		opts = {},
 		version = false
 	},
-	{ -- dressing
-		"stevearc/dressing.nvim",
-		event = "VeryLazy",
-	},
-	{ -- mini.animate
-		"echasnovski/mini.animate",
-		opts = {},
-	},
 }
+
