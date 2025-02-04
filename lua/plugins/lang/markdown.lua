b2f6c2ec790c01960e811f6c215e42cfe2d81106
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-02-03 22:38:36 -0500

    Update markdown module
    Replace headlines with render-markdown. Add marksman to lspconfig. Add
    conform and nvim-lint spec deploying prettier and markdownlint resp.

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,35 +1,53 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
-			end
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "folke/snacks.nvim" },
+		opts = {},
+		ft = { "markdown", "norg", "rmd", "org" },
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+			require("snacks")
+				.toggle({
+					name = "Render Markdown",
+					get = function()
+						return require("render-markdown.state").enabled
+					end,
+					set = function(enabled)
+						local m = require("render-markdown")
+						if enabled then
+							m.enable()
+						else
+							m.disable()
+						end
+					end,
+				})
+				:map("\\M")
 		end,
 	},
 	{
-		"lukas-reineke/headlines.nvim",
-		opts = function()
-			local opts = {}
-			for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
-				opts[ft] = {
-					headline_highlights = {},
-				}
-				for i = 1, 6 do
-					local hl = "Headline" .. i
-					vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
-					table.insert(opts[ft].headline_highlights, hl)
-				end
-			end
-			return opts
-		end,
-		ft = { "markdown", "norg", "rmd", "org" },
-		config = function(_, opts)
-			-- PERF: schedule to prevent headlines slowing down opening a file
-			vim.schedule(function()
-				require("headlines").setup(opts)
-				require("headlines").refresh()
-			end)
-		end,
+		"mfussenegger/nvim-lint",
+		optional = true,
+		opts = {
+			linters_by_ft = {
+				markdown = { "markdownlint-cli2" },
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		optional = true,
+		opts = {
+			servers = {
+				marksman = {},
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				markdown = { "prettier" },
+			},
+		},
 	},
 }
