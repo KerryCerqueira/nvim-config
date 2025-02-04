52531f11d6af76c643ca5cecc3d40477a9e14956
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-02-03 20:33:46 -0500

    Update init.lua to reflect removal or e.g. keymap.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
-vim.cmd("cmap w!! w !sudo tee > /dev/null %")
-vim.api.nvim_create_user_command("Xs", "mks! | xa", { force = true })
+vim.g.localleader = ","
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -12,29 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 		lazypath,
 	})
 end
-local keymaps = require("keymaps")
-keymaps.set_keymaps(keymaps.user_keymaps)
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{import = "plugins"},
 	{import = "plugins.lang"},
 })
-vim.opt.shiftwidth = 3
-vim.opt.tabstop = 3
-vim.opt.signcolumn = "yes"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.cmd('syntax enable')
-vim.filetype.add({
-  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-})
-if vim.g.neovide then
-	vim.g.neovide_scale_factor = 0.85
-	vim.g.neovide_transparency = 0.9
-	vim.g.neovide_floating_blur_amount_x = 0
-	vim.g.neovide_floating_blur_amount_y = 0
-	vim.g.neovide_floating_shadow = true
-	vim.g.neovide_floating_z_height = 10
-	vim.g.neovide_light_angle_degrees = 45
-	vim.g.neovide_light_radius = 5
-end
