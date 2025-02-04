8fb08ab4735a9059751e1e029505da0efc4c65cb
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-02-03 22:33:27 -0500

    Update which-key module
    Which-key module re-written at some point pushing many breaking changes.
    Add hydra mode keybind for window resizing. Add keybind to show
    keybinds. Add group prefixes for various plugins.
    
    opts_extend mechanism seems to be broken by this spec should be broken
    up and integrated into lang and integration concerns for other modules.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,44 @@
-return { -- which-key
+return {
 	"folke/which-key.nvim",
+	dependencies = { "echasnovski/mini.icons" },
 	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show()
+			end,
+			desc = "Show Keymaps (which-key)",
+		},
+		{
+			"<c-w><space>",
+			function()
+				require("which-key").show({ keys = "<c-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode (which-key)",
+		},
+	},
+	opts = {
+		preset = "modern",
+		spec = {
+			{ "<leader>a", group = "ai" },
+			{ "<leader>b", group = "bufferline" },
+			{ "<leader>f", group = "find..." },
+			{ "<leader>G", group = "git" },
+			{ "<Leader>x", group = "trouble" },
+			{ "<Leader>s", group = "surround" },
+			{ "<Leader>m", group = "minimap" },
+			{ "<Leader>n", group = "notifications" },
+			{ "\\", group = "toggle" },
+			{ "\\t", group = "treesitter" },
+			{ "\\m", group = "minimap" },
+			{ "]", group = "iteration" },
+			{ "[", group = "reverse iteration" },
+			{ "<Localleader>l", group = "vimtex" },
+		},
+	},
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
 	end,
-	opts = {
-		plugins = { spelling = true },
-	},
 }
