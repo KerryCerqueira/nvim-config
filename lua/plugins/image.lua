7a590704b318a04ce2eec1ddc75d80bd836cf923
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-02-03 20:59:07 -0500

    Add image.nvim module
    Add image nvim configuration, lazyloading on buffer reads of images and
    markdown files.

---
+++ lua/plugins/image.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"3rd/image.nvim",
+		event = {
+			"BufReadPre *.png",
+			"BufReadPre *.jpg",
+			"BufReadPre *.jpeg",
+			"BufReadPre *.gif",
+			"BufReadPre *.webp",
+			"BufReadPre *.avif"
+		},
+		ft = { "markdown", "vimwiki", "norg" },
+		opts = {}
+	}
+}
