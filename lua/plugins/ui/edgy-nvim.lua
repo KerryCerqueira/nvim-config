41c2b2f9da5f6eaff3e924ddc40825a447d2cf12
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-09-04 06:38:14 -0400

    Split edgy spec
    Split edgy plugin spec into plugin specific dependencies. Add type
    hinting to edgy spec.

--- lua/plugins/ui/edgy-nvim.lua
+++ lua/plugins/ui/edgy-nvim.lua
@@ -1,3 +1,10 @@
+if false then
+	require("lazy")
+	require("which-key")
+	require("edgy")
+end
+
+---@type LazySpec
 return {
 	"folke/edgy.nvim",
 	event = "VeryLazy",
@@ -6,33 +13,9 @@ return {
 		vim.opt.splitkeep = "screen"
 	end,
 	opts_extend = { "bottom", "left", "right" },
-	---@module 'edgy'
-	---@param opts Edgy.Config
+	---@type Edgy.Config
 	opts = {
 		bottom = {
-			{
-				title = "Diagnostics",
-				ft = "trouble",
-				filter = function(_, win)
-					return vim.w[win].trouble
-						and vim.w[win].trouble.position == "bottom"
-						and vim.w[win].trouble.type == "split"
-						and vim.w[win].trouble.relative == "editor"
-						and vim.w[win].trouble.mode == "diagnostics"
-						and not vim.w[win].trouble_preview
-				end,
-			},
-			{
-				title = "%{b:snacks_terminal.id}: %{b:term_title}",
-				ft = "snacks_terminal",
-				size = { height = 8 },
-				filter = function(_, win)
-					return vim.w[win].snacks_win
-						and vim.w[win].snacks_win.position == "bottom"
-						and vim.w[win].snacks_win.relative == "editor"
-						and not vim.w[win].trouble_preview
-				end,
-			},
 			{ ft = "qf", title = "QuickFix" },
 			{
 				ft = "markdown",
@@ -56,52 +39,6 @@ return {
 				-- only show help buffers
 				filter = function(buf)
 					return vim.bo[buf].buftype == "help"
-				end,
-			},
-		},
-		left = {
-			{
-				title = "LSP Defs/Refs/Decs",
-				ft = "trouble",
-				filter = function(_, win)
-					return vim.w[win].trouble
-						and vim.w[win].trouble.position == "left"
-						and vim.w[win].trouble.type == "split"
-						and vim.w[win].trouble.relative == "editor"
-						and vim.w[win].trouble.mode == "lsp"
-						and not vim.w[win].trouble_preview
-				end,
-			},
-			{
-				title = "",
-				ft = "trouble",
-				filter = function(_, win)
-					return vim.w[win].trouble
-						and vim.w[win].trouble.position == "left"
-						and vim.w[win].trouble.type == "split"
-						and vim.w[win].trouble.relative == "editor"
-						and vim.w[win].trouble.mode == "symbols"
-						and not vim.w[win].trouble_preview
-				end,
-			},
-			{
-				title = "Neo-Tree Buffers",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "buffers"
-				end,
-				open = function()
-					vim.cmd("Neotree show position=top buffers dir=%s", vim.fn.getcwd())
-				end,
-			},
-		},
-		right = {
-			{
-				ft = "codecompanion",
-				title = "AI chat",
-				size = { width = 70 },
-				filter = function(_, win)
-					return vim.api.nvim_win_get_config(win).relative == ""
 				end,
 			},
 		},
--- lua/plugins/ui/snacks-nvim.lua
+++ lua/plugins/ui/snacks-nvim.lua
@@ -213,4 +213,51 @@ return {
 			},
 		},
 	},
+	{
+		"folke/edgy.nvim",
+		optional = true,
+		opts_extend = { "bottom", "left", "right" },
+		---@type Edgy.Config
+		opts = {
+			bottom = {
+				{
+					title = "%{b:snacks_terminal.id}: %{b:term_title}",
+					ft = "snacks_terminal",
+					size = { height = 8 },
+					filter = function(_, win)
+						return vim.w[win].snacks_win
+							and vim.w[win].snacks_win.position == "bottom"
+							and vim.w[win].snacks_win.relative == "editor"
+							and not vim.w[win].trouble_preview
+					end,
+				},
+			},
+			left = {
+				{
+					title = "LSP Defs/Refs/Decs",
+					ft = "trouble",
+					filter = function(_, win)
+						return vim.w[win].trouble
+							and vim.w[win].trouble.position == "left"
+							and vim.w[win].trouble.type == "split"
+							and vim.w[win].trouble.relative == "editor"
+							and vim.w[win].trouble.mode == "lsp"
+							and not vim.w[win].trouble_preview
+					end,
+				},
+				{
+					title = "",
+					ft = "trouble",
+					filter = function(_, win)
+						return vim.w[win].trouble
+							and vim.w[win].trouble.position == "left"
+							and vim.w[win].trouble.type == "split"
+							and vim.w[win].trouble.relative == "editor"
+							and vim.w[win].trouble.mode == "symbols"
+							and not vim.w[win].trouble_preview
+					end,
+				},
+			},
+		},
+	},
 }
