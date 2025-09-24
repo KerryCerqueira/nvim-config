a4e19d35dcd175b78eb59c1f78b832f0c9cd422f
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-09-24 15:53:45 -0400

    Fix snacks.image dependencies

--- nix/plugins/ui/snacks-nvim.nix
+++ nix/plugins/ui/snacks-nvim.nix
@@ -1,7 +1,24 @@
 { pkgs, ... }:
-
-{
-	programs.neovim.plugins = [ pkgs.vimPlugins.snacks-nvim ];
+let
+	tex = pkgs.texlive.combine {
+		inherit (pkgs.texlive)
+		preview
+		standalone
+		varwidth
+		scheme-small;
+	};
+in {
+	programs.neovim = {
+		plugins = with pkgs.vimPlugins; [
+			snacks-nvim
+		];
+		extraPackages = with pkgs; [
+			ghostscript
+			imagemagick
+			mermaid-cli
+			tex
+		];
+	};
 	xdg.configFile = {
 		"nvim/lua/plugins/ui/snacks-nvim.lua".source =
 			../../../lua/plugins/ui/snacks-nvim.lua;
