1683135166a5ac265cf9b9799908ec473a65bfae
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-10-07 14:34:48 -0400

    Fix sshfs config
    Add configs from .ssh/config.d to sshfs.nvim detection. Fixes issue with
    includes not working with this plugin

--- lua/plugins/ui/sshfs-nvim.lua
+++ lua/plugins/ui/sshfs-nvim.lua
@@ -6,6 +6,20 @@ end
 return {
 	"uhs-robert/sshfs.nvim",
 	opts = {
+		connections = {
+			ssh_configs = vim.list_extend(
+				{
+					vim.fn.expand("$HOME" .. "/.ssh/config"),
+					"/etc/ssh_config",
+				},
+				vim.fn.globpath(
+					vim.fn.expand("$HOME" .. "/.ssh/config.d"),
+					"*",
+					false,
+					true
+				)
+			)
+		},
 		lead_prefix = "<leader>M",
 		ui = {
 			file_picker = {
