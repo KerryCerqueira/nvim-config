156ddb905b1a1260d5915918441835de1928992a
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-10-29 13:38:21 -0400

    Add secrets initialization to config

--- init.lua
+++ init.lua
@@ -2,5 +2,7 @@ require("options").setup()
 require("lazy-setup").setup()
 require("keymaps").setup()
 require("autocommands").setup()
-local ok, extra_config = pcall(require, "extra-config")
-if ok then extra_config.setup() end
+local secrets_ok, secrets = pcall(require, "secrets")
+if secrets_ok then secrets.setup() end
+local extra_ok, extra_config = pcall(require, "extra-config")
+if extra_ok then extra_config.setup() end
